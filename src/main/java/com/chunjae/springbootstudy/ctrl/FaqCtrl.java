74f5d2efde8bde107ecc9a015d114162748165d6
jinkwonpark <jk3473@naver.com>
2023-11-14 13:12:53 +0900

    faq 완성

--- src/main/java/com/chunjae/springbootstudy/ctrl/FaqCtrl.java
+++ src/main/java/com/chunjae/springbootstudy/ctrl/FaqCtrl.java
@@ -1,16 +1,27 @@
 package com.chunjae.springbootstudy.ctrl;
 
+import com.chunjae.springbootstudy.entity.Faq;
 import com.chunjae.springbootstudy.service.FaqService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/faq/*")
 public class FaqCtrl {
+
     @Autowired
     private FaqService faqService;
 
-    @GetMapping
+    // FAQ
+    @GetMapping("faqList")
+    public String faqList(Model model) {
+        List<Faq> faqList = faqService.faqList();
+        model.addAttribute("faqList", faqList);
+        return "faq/faqList";
+    }
 }
